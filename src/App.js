4924a6f9359438f7c6fc6b62b1da0e15722a3aea
philline011 <106289562+philline011@users.noreply.github.com>
2022-05-28 18:29:03 +0800

    Added Collatz Conjecture function
    Installed react-chartjs-2 and deleted some files bcos I did not know may laman pala. Retrieved them again and updated App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,71 @@
-import logo from './logo.svg'; 
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import Highcharts from 'highcharts';
-import HighchartsReact from 'highcharts-react-official';
+import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
 
 const App = () => {
   const [number, setNumber] = useState(0);
   const [sequence, setSequence] = useState([]);
   const options = {
     chart: {
-      type: 'spline'
+      type: "spline",
     },
     title: {
-      text: 'My chart'
+      text: "My chart",
     },
     series: [
       {
-        data: sequence
-      }
-    ]
+        data: sequence,
+      },
+    ],
   };
 
   useEffect(() => {
     // Start coding here
+    let collatzConjectureData = [];
+
+    function getCollatzConjecture(num) {
+      let len = collatzConjectureData.length;
+      collatzConjectureData = collatzConjectureData.concat(num);
+
+      if (num === 0) return 0;
+      else if (num === 1) return 1;
+      else if (num % 2 === 0) return getCollatzConjecture(num / 2);
+      else return getCollatzConjecture(num * 3 + 1);
+    }
+
+    return () => {
+      console.log(number);
+      let num = number;
+
+      //check if input is an integer
+      let x = parseFloat(num);
+      if (!isNaN(num) && (x | 0) === x) {
+        num = parseInt(num);
+        collatzConjectureData = [];
+        getCollatzConjecture(num);
+      }
+
+      setSequence(collatzConjectureData);
+    };
 
     // End coding here
   }, [number]);
 
   return (
     <div className="App">
-      <input type={"number"} onChange={(e)=> {
-        setNumber(e.target.value)
-      }}/>
+      <input
+        type={"number"}
+        onChange={(e) => {
+          setNumber(e.target.value);
+        }}
+      />
       <div>
         <HighchartsReact highcharts={Highcharts} options={options} />
       </div>
     </div>
   );
-}
+};
 
 export default App;
